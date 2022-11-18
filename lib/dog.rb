07bf2b792c2ebd4156c1b78bc56198b8f87e5294
Tim <timtran007@gmail.com>
2022-11-18 00:14:07 -0600

    commit tree 27de12749d843a77c5dd42860b59008408c09025

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
+require "pry"
 class Dog
+    def name= (name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed= (breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+dog1 = Dog.new
+dog1.name = "Rex"
+dog1.breed = "GSD"
+
+binding.pry
